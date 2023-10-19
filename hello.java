64ae3d64be32e4db928ef5bbfa7e96a87bac57f5
mohqamar <mqamar@umich.edu>
2023-10-18 20:30:48 -0400

    initialcommit

---
+++ hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+	System.out.println("Notepad is the best IDE to write code in :)");
+        for(int i=1;i<=12;i++) {
+
+            System.out.println("Hello World..."+i);
+        }
+    }
+}
